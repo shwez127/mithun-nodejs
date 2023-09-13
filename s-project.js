52de82c0e7e88d95085c3f2d678d57e62cdad57f
shwez127 <121848650+shwez127@users.noreply.github.com>
2023-09-13 16:52:48 +0530

    Update s-project.js

--- s-project.js
+++ s-project.js
@@ -1,19 +1,19 @@
-const sonarqubeScanner = require('sonarqube-scanner');
+// const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
-  serverUrl: 'http://172.17.0.3:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_a96d5c8a3bc500845578de0e4dae775a95518fcf',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
-}, () => {});
+// sonarqubeScanner({
+//   serverUrl: 'http://172.17.0.3:9000/',
+//        options : {
+// 	    'sonar.projectDescription': 'This is a Node JS application',
+// 	    'sonar.projectName': 'Node JS Application - Sample',
+// 	    'sonar.projectKey':'NodeJsMithunTechnologies',
+// 	    'sonar.login': 'squ_a96d5c8a3bc500845578de0e4dae775a95518fcf',
+// 	    //'sonar.login': 'admin',
+// 	    //'sonar.password': 'admin',
+//             'sonar.projectVersion':'1.0',
+// 	    'sonar.language':'js',
+//             'sonar.sourceEncoding':'UTF-8',
+//             'sonar.sources': '.',
+// 	  //'sonar.tests': 'specs',
+//           //'sonar.inclusions' : 'src/**'
+//        },
+// }, () => {});
